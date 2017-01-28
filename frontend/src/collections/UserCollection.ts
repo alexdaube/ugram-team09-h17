a388f6909a7998af267c5d13c64fec33cf3b57ac
Léandre Noel <leandre.noel.1@ulaval.ca>
2017-01-28 13:44:05 -0500

    forget a merge conflit in the previous commit

--- frontend/src/collections/UserCollection.ts
+++ frontend/src/collections/UserCollection.ts
@@ -1,17 +1,10 @@
 /**
  * Created by jeromelabonte on 2017-01-21.
  */
-<<<<<<< HEAD:frontend/src/collections/UserCollection.ts
 import {UserModel} from '../models/UserModel';
 import * as Backbone from "backbone"
 
 export class UserCollection extends Backbone.Collection<UserModel>{
-=======
-import * as Backbone from "backbone";
-import {ProfileModel} from "../models/ProfileModel";
-
-export class ProfileCollection extends Backbone.Collection<ProfileModel> {
->>>>>>> develop:frontend/src/collections/ProfileCollection.ts
     constructor(options?: any) {
         super(options);
         this.model = UserModel;
