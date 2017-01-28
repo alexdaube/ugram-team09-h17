488cda64dc9343264a4db09017ccb811b608a140
Francois <nate@nate.fr>
2017-01-28 13:09:48 -0500

    Fixed more TSLint warnings

--- frontend/src/util/StringFormatter.ts
+++ frontend/src/util/StringFormatter.ts
@@ -1,19 +1,19 @@
 export module StringFormatter {
-     export function formatMillisecondDateToMMDDYYYY(date: Date): string {
+    export function formatMillisecondDateToMMDDYYYY(date: Date): string {
 
         let dd = date.getDate().toString();
-        let mm = date.getMonth() + 1;
+        const mm = date.getMonth() + 1;
         let mm1 = mm.toString();
-        let yyyy = date.getFullYear().toString();
+        const yyyy = date.getFullYear().toString();
 
-        if(dd.length < 2) {
+        if (dd.length < 2) {
             dd = "0" + dd;
         }
-        if(mm1.length < 2) {
+        if (mm1.length < 2) {
             mm1 = "0" + mm1;
         }
 
-         let formattedDate = mm1.toString() + "/" + dd.toString() + "/" + yyyy.toString();
+        const formattedDate = mm1.toString() + "/" + dd.toString() + "/" + yyyy.toString();
         return formattedDate;
     }
 }
