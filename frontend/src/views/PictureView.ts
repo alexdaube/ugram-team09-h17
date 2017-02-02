d2a98ef05357795a0bd25a37dbc78b56bd5d7e23
Alex Aube <alexandre.desilets-aube.1@ulaval.ca>
2017-02-02 16:31:41 -0500

    Solving link problem from picture in profile to feed page

--- frontend/src/views/PictureView.ts
+++ frontend/src/views/PictureView.ts
@@ -15,7 +15,7 @@ export class PictureView extends Backbone.View<PictureModel> {
     public render() {
         this.model.fetch({
             success: () => {
-                this.$el.html(this.template({picture: this.model, user: UserModel}));
+                this.$el.html(this.template({picture: this.model}));
             },
             error: () => {
                 this.$el.html("There was an error");
@@ -25,7 +25,7 @@ export class PictureView extends Backbone.View<PictureModel> {
     }
 
     public append() {
-        this.$el.append(this.template({picture: this.model, user: UserModel}));
+        this.$el.append(this.template({picture: this.model}));
         return this;
     }
 }
