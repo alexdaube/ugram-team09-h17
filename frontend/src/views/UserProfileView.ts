dcecd0e257df33dc8bd6802fc92638fbf2c0bf4e
Alex Aube <alexandre.desilets-aube.1@ulaval.ca>
2017-02-02 13:16:40 -0500

    Removing dead code

--- frontend/src/views/UserProfileView.ts
+++ frontend/src/views/UserProfileView.ts
@@ -1,7 +1,5 @@
 import * as Backbone from "backbone";
-import * as _ from "underscore";
 
-import {UserModel} from "../models/UserModel";
 import {PictureView} from "./PictureView";
 import {ShowMoreView} from "./ShowMoreView";
 
