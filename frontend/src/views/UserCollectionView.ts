025dc5b1bb8866008015b7bf8c63c235d24631bc
Mathieu Vézina <Mathieu Vzina>
2017-02-12 19:49:49 -0500

    search function

--- frontend/src/views/UserCollectionView.ts
+++ frontend/src/views/UserCollectionView.ts
@@ -1,13 +1,17 @@
 import * as Backbone from "backbone";
+import * as $ from "jquery";
 import * as _ from "underscore";
 
 import {ShowMoreView} from "./ShowMoreView";
 import {UserView} from "./UserView";
+import {SearchView} from "./SearchView";
 
 export class UserCollectionView extends Backbone.View<any> {
     private template: Function;
     private usersPerPage: number = 15;
     private nextPageToFetch: number = 0;
+    private usersPerPageSearch: number = 100000;
+    private searchText: string = "";
 
     constructor(options?: Backbone.ViewOptions<any>) {
         super(_.extend({el: "#content"}, options));
@@ -15,13 +19,35 @@ export class UserCollectionView extends Backbone.View<any> {
     }
 
     public render() {
+
+        $("document").ready( function() {
+            $(".searchBox").hide();
+
+            $("#content").click( function() {
+                $(".searchBox").hide();
+            });
+
+            $("#findInput").click( function(event) {
+                $(".searchBox").show();
+                event.stopPropagation(); 
+            });
+
+            $("#findInput").keyup( function(event) {
+                this.searchText = $("#findInput").val();
+                console.log(this.searchText);
+            });
+        });
+
         const html = this.template();
         this.$el.html(html);
         this.showPictures();
+        this.showSearch();
+
         const showMoreView = new ShowMoreView({
             el: "#show-more-container",
             showMoreCallback: this.showPictures.bind(this),
         });
+
         showMoreView.render();
         return this;
     }
@@ -39,6 +65,19 @@ export class UserCollectionView extends Backbone.View<any> {
         });
     }
 
+    private showSearch() {
+        this.collection.fetch({
+            data: {
+                page: this.nextPageToFetch,
+                perPage: this.usersPerPageSearch,
+            },
+            success: () => {
+                this.nextPageToFetch += 1;
+                this.renderSearch();
+            },
+        });
+    }
+
     private renderPictures() {
         this.collection.each((user) => {
             const userView = new UserView({el: "#usersList" , model: user});
@@ -51,5 +90,20 @@ export class UserCollectionView extends Backbone.View<any> {
         if (this.collection.length < this.usersPerPage) {
             $("#show-more-container").hide();
         }
+    }
+
+    private renderSearch() {
+        //var compter = 0;
+        this.collection.each((user) => {
+            console.log("1 :" + user.id);
+            console.log("2 :" + this.searchText);
+            if (user.id == this.searchText) {
+                 const searchView = new SearchView({el: "#searchList" , model: user});
+                 searchView.append();
+            //     //compter++;
+            }
+        });
+
+        //console.log(compter);
     }
 }
