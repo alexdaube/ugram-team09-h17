803faa608713c199acfd7c74eb57551a05aeac96
Francois <nate@nate.fr>
2017-03-19 14:43:09 -0400

    Enforcing https api

--- frontend/constants/prod.ts
+++ frontend/constants/prod.ts
@@ -2,6 +2,6 @@
 // const API_PORT = "3000";
 
 // export const API_BASE_URL =`${API_HOST}:${API_PORT}/`;
-export const API_BASE_URL = "http://api.aide.moe/";
+export const API_BASE_URL = "https://api.aide.moe/";
 
 export const FB_CLIENT = "755479644628950";
